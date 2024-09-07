ddc6eba49451ef91d296863b22609ca1bda5e96b
Helbert Vieira <helbert.vieira@cmdnet.com>
2024-09-07 18:22:48 -0300

    requirements atualizado

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app=Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-app.config["SECRET_KEY"] = "79d66f3e1defe9f4a71bf7cc6b5641fa"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASDE_URL")
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
