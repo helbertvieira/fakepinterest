7765a7f9a69851aea779faedbcdb1f4f7d933e97
Helbert Vieira <helbert.vieira@cmdnet.com>
2024-09-07 18:24:42 -0300

    mudança banco postgree

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -4,8 +4,10 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
 
+
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASDE_URL")
+app.config["SECRET_KEY"] = "79d66f3e1defe9f4a71bf7cc6b5641fa"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
